a8694d365029a53e3c69b1532a2aec074a41fa3b
royyandzakiy <royyan.dzakiy@efishery.com>
2021-02-04 08:37:26 +0700

    changed gitignore:

--- src/main.cpp
+++ src/main.cpp
@@ -145,41 +145,7 @@ void loop() {
 //======================================================================//
 // Functions
 
-void mqttCallback(char* topic, byte* payload, unsigned int len) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("]: ");
-  Serial.write(payload, len);
-  Serial.println();
-
-  // Only proceed if incoming message's topic matches
-  if (String(topic) == topicLed) {
-    ledStatus = !ledStatus;
-    digitalWrite(LED_PIN, ledStatus);
-    mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
-  }
-}
-
-boolean mqttConnect() {
-  Serial.print("Connecting to ");
-  Serial.print(broker);
-
-  // Connect to MQTT Broker without authentication
-  // boolean status = mqtt.connect("Aquifera-ESP32-Client");
-
-  // Or, if you want to authenticate MQTT:
-  boolean status = mqtt.connect("Aquifera-ESP32-Client", mqtt_user, mqtt_pass);
-
-  if (status == false) {
-    Serial.println(" fail");
-    return false;
-  }
-  Serial.println(" success");
-  mqtt.publish(topicInit, "GsmClientTest started");
-  mqtt.subscribe(topicLed);
-  return mqtt.connected();
-}
-
+// TinyGSM
 void setup_tinygsm() {
   // Set GSM module baud rate
   TinyGsmAutoBaud(SerialSim800L, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
@@ -248,12 +214,7 @@ void setup_tinygsm() {
 #endif
 }
 
-void setup_mqtt() {
-  // MQTT Broker setup
-  mqtt.setServer(broker, 1883);
-  mqtt.setCallback(mqttCallback);
-}
-
+// WiFi
 void setup_wifi() {
   WiFi.begin(ssid, password);
  
@@ -261,4 +222,46 @@ void setup_wifi() {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
+}
+
+// MQTT
+void setup_mqtt() {
+  // MQTT Broker setup
+  mqtt.setServer(broker, 1883);
+  mqtt.setCallback(mqttCallback);
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int len) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.write(payload, len);
+  Serial.println();
+
+  // Only proceed if incoming message's topic matches
+  if (String(topic) == topicLed) {
+    ledStatus = !ledStatus;
+    digitalWrite(LED_PIN, ledStatus);
+    mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
+  }
+}
+
+boolean mqttConnect() {
+  Serial.print("Connecting to ");
+  Serial.print(broker);
+
+  // Connect to MQTT Broker without authentication
+  // boolean status = mqtt.connect("Aquifera-ESP32-Client");
+
+  // Or, if you want to authenticate MQTT:
+  boolean status = mqtt.connect("Aquifera-ESP32-Client", mqtt_user, mqtt_pass);
+
+  if (status == false) {
+    Serial.println(" fail");
+    return false;
+  }
+  Serial.println(" success");
+  mqtt.publish(topicInit, "GsmClientTest started");
+  mqtt.subscribe(topicLed);
+  return mqtt.connected();
 }
