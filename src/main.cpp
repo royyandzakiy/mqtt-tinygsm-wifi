ec944d49d928d4b82d90c78d6abf74de437986de
royyandzakiy <royyan.dzakiy@efishery.com>
2021-02-04 08:20:34 +0700

    added pubsubclient, refactored by adding better commentary

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,45 @@
-/**************************************************************
- *
- * For this example, you need to install PubSubClient library:
- *   https://github.com/knolleary/pubsubclient
- *   or from http://librarymanager/all#PubSubClient
- *
- * TinyGSM Getting Started guide:
- *   https://tiny.cc/tinygsm-readme
- *
- * For more MQTT examples, see PubSubClient library
- *
- **************************************************************
- * Use Mosquitto client tools to work with MQTT
- *   Ubuntu/Linux: sudo apt-get install mosquitto-clients
- *   Windows:      https://mosquitto.org/download/
- *
- * Subscribe for messages:
- *   mosquitto_sub -h test.mosquitto.org -t GsmClientTest/init -t GsmClientTest/ledStatus -q 1
- * Toggle led:
- *   mosquitto_pub -h test.mosquitto.org -t GsmClientTest/led -q 1 -m "toggle"
- *
- * You can use Node-RED for wiring together MQTT-enabled devices
- *   https://nodered.org/
- * Also, take a look at these additional Node-RED modules:
- *   node-red-contrib-blynk-ws
- *   node-red-dashboard
- *
- **************************************************************/
-
-// Select your modem:
-#define TINY_GSM_MODEM_SIM800
-// #define TINY_GSM_MODEM_SIM808
-// #define TINY_GSM_MODEM_SIM868
-// #define TINY_GSM_MODEM_SIM900
-// #define TINY_GSM_MODEM_SIM7000
-// #define TINY_GSM_MODEM_SIM5360
-// #define TINY_GSM_MODEM_SIM7600
-// #define TINY_GSM_MODEM_UBLOX
-// #define TINY_GSM_MODEM_SARAR4
-// #define TINY_GSM_MODEM_M95
-// #define TINY_GSM_MODEM_BG96
-// #define TINY_GSM_MODEM_A6
-// #define TINY_GSM_MODEM_A7
-// #define TINY_GSM_MODEM_M590
-// #define TINY_GSM_MODEM_MC60
-// #define TINY_GSM_MODEM_MC60E
-// #define TINY_GSM_MODEM_ESP8266
-// #define TINY_GSM_MODEM_XBEE
-// #define TINY_GSM_MODEM_SEQUANS_MONARCH
-
-// Set serial for debug console (to the Serial Monitor, default speed 115200)
-#define SerialMon Serial
-
-// Set serial for AT commands (to the module)
-// Use Hardware Serial on Mega, Leonardo, Micro
-//#define SerialAT Serial1
-
-// or Software Serial on Uno, Nano
-#include <SoftwareSerial.h>
-//SoftwareSerial SerialAT(2, 3); // RX, TX
-SoftwareSerial SerialAT(10, 9);
-
-// See all AT commands, if wanted
-// #define DUMP_AT_COMMANDS
-
-// Define the serial console for debug prints, if needed
-#define TINY_GSM_DEBUG SerialMon
-
-// Range to attempt to autobaud
-#define GSM_AUTOBAUD_MIN 9600
-#define GSM_AUTOBAUD_MAX 115200
-
-// Add a reception delay - may be needed for a fast processor at a slow baud rate
-// #define TINY_GSM_YIELD() { delay(2); }
-
-// Define how you're planning to connect to the internet
-#define TINY_GSM_USE_GPRS true
-#define TINY_GSM_USE_WIFI false
-
-// set GSM PIN, if any
-#define GSM_PIN ""
-
+// *** main.cpp ***
+#include <TinyGsmClient.h>
+#include <PubSubClient.h>
+#include <EspSoftwareSerial.h>
 #include "credentials.h"
+
+//======================================================================//
+// CONFIGURATIONS
+
+// #define CELLULAR_ONLY // uncomment if want to use SIM800L as connector to internet
+#define WIFI_ONLY // uncomment if want to use WiFi as connector to internet
+
+// ***** TINY GSM CONFIGURATIONS *****
+// See all AT commands, if wanted
+#define DUMP_AT_COMMANDS
 
 // Your GPRS credentials, if any
 #ifndef CREDENTIALS_H
   const char apn[] = "APN";
   const char gprsUser[] = "APN_GPRS_USER";
   const char gprsPass[] = "APN_GPRS_PASS";
-
-  // Your WiFi connection credentials, if applicable
-  const char wifiSSID[] = "WIFI_SSID";
-  const char wifiPass[] = "WIFI_PASS";
-
-  // MQTT details
-  const char* broker = "MQTT_BROKER_SERVER";
-  const char* mqtt_user = "MQTT_BROKER_USER";
-  const char* mqtt_pass = "MQTT_BROKER_PASS";
 #endif
 
-const char* topicLed = "GsmClientTest/led";
-const char* topicInit = "GsmClientTest/init";
-const char* topicLedStatus = "GsmClientTest/ledStatus";
+// set GSM PIN, if any
+#define GSM_PIN ""
 
-#include <TinyGsmClient.h>
-#include <PubSubClient.h>
+// Select your modem:
+#define TINY_GSM_MODEM_SIM800
+
+// Software Serial to SIM800L Modem
+SoftwareSerial SerialSim800L(10, 9); // RX, TX
+
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG Serial
+
+// Range to attempt to autobaud
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 115200
+
+// Define how you're planning to connect to the internet
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
 
 // Just in case someone defined the wrong thing..
 #if TINY_GSM_USE_GPRS && not defined TINY_GSM_MODEM_HAS_GPRS
@@ -121,143 +57,76 @@ const char* topicLedStatus = "GsmClientTest/ledStatus";
 
 #ifdef DUMP_AT_COMMANDS
   #include <StreamDebugger.h>
-  StreamDebugger debugger(SerialAT, SerialMon);
+  StreamDebugger debugger(SerialSim800L, Serial);
   TinyGsm modem(debugger);
 #else
-TinyGsm modem(SerialAT);
+TinyGsm modem(SerialSim800L);
 #endif
-TinyGsmClient client(modem);
+
+// ***** WIFI & MQTT CONFIGURATIONS *****
+// WIFI & MQTT Credentials
+#ifndef CREDENTIALS_H
+  // Your WiFi connection credentials, if applicable
+  const char wifiSSID[] = "WIFI_SSID";
+  const char wifiPass[] = "WIFI_PASS";
+
+  // MQTT details
+  const char* broker = "MQTT_BROKER_SERVER";
+  const char* mqtt_user = "MQTT_BROKER_USER";
+  const char* mqtt_pass = "MQTT_BROKER_PASS";
+#endif
+
+const char* topicLed = "GsmClientTest/led";
+const char* topicInit = "GsmClientTest/init";
+const char* topicLedStatus = "GsmClientTest/ledStatus";
+
+// ****** GLOBAL VARIABLES *****
+#ifdef CELLULAR_ONLY
+  TinyGsmClient client(modem);
+#else defined(WIFI_ONLY)
+  #include <WiFi.h>
+  WiFiClient client;
+#endif
+
 PubSubClient mqtt(client);
 
+// ****** OTHER VARIABLES *****
 #define LED_PIN 13
 int ledStatus = LOW;
 
 uint32_t lastReconnectAttempt = 0;
 
-void mqttCallback(char* topic, byte* payload, unsigned int len) {
-  SerialMon.print("Message arrived [");
-  SerialMon.print(topic);
-  SerialMon.print("]: ");
-  SerialMon.write(payload, len);
-  SerialMon.println();
+//======================================================================//
+// Prototypes
 
-  // Only proceed if incoming message's topic matches
-  if (String(topic) == topicLed) {
-    ledStatus = !ledStatus;
-    digitalWrite(LED_PIN, ledStatus);
-    mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
-  }
-}
+void mqttCallback(char*, byte*, unsigned int);
+boolean mqttConnect();
+void setup_tinygsm();
+void setup_mqtt();
 
-boolean mqttConnect() {
-  SerialMon.print("Connecting to ");
-  SerialMon.print(broker);
-
-  // Connect to MQTT Broker
-  // boolean status = mqtt.connect("GsmClientTest");
-
-  // Or, if you want to authenticate MQTT:
-  boolean status = mqtt.connect(broker, mqtt_user, mqtt_pass);
-
-  if (status == false) {
-    SerialMon.println(" fail");
-    return false;
-  }
-  SerialMon.println(" success");
-  mqtt.publish(topicInit, "GsmClientTest started");
-  mqtt.subscribe(topicLed);
-  return mqtt.connected();
-}
-
+//======================================================================//
+// Main
 
 void setup() {
   // Set console baud rate
-  SerialMon.begin(9600);
+  Serial.begin(9600);
   delay(10);
 
   pinMode(LED_PIN, OUTPUT);
 
-  // !!!!!!!!!!!
-  // Set your reset, enable, power pins here
-  // !!!!!!!!!!!
+  Serial.println("Wait...");
 
-  SerialMon.println("Wait...");
-
-  // Set GSM module baud rate
-  TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
-  // SerialAT.begin(9600);
-  delay(6000);
-
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  SerialMon.println("Initializing modem...");
-  modem.restart();
-  // modem.init();
-
-  String modemInfo = modem.getModemInfo();
-  SerialMon.print("Modem Info: ");
-  SerialMon.println(modemInfo);
-
-#if TINY_GSM_USE_GPRS
-  // Unlock your SIM card with a PIN if needed
-  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
-    modem.simUnlock(GSM_PIN);
-  }
+#ifdef CELLULAR_ONLY
+  setup_tinygsm();
+#else defined(WIFI_ONLY)
+  setup_wifi();
 #endif
-
-#if TINY_GSM_USE_WIFI
-    // Wifi connection parameters must be set before waiting for the network
-  SerialMon.print(F("Setting SSID/password..."));
-  if (!modem.networkConnect(wifiSSID, wifiPass)) {
-    SerialMon.println(" fail");
-    delay(10000);
-    return;
-  }
-  SerialMon.println(" success");
-#endif
-
-#if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_XBEE
-  // The XBee must run the gprsConnect function BEFORE waiting for network!
-  modem.gprsConnect(apn, gprsUser, gprsPass);
-#endif
-
-  SerialMon.print("Waiting for network...");
-  if (!modem.waitForNetwork()) {
-    SerialMon.println(" fail");
-    delay(10000);
-    return;
-  }
-  SerialMon.println(" success");
-
-  if (modem.isNetworkConnected()) {
-    SerialMon.println("Network connected");
-  }
-
-#if TINY_GSM_USE_GPRS
-  // GPRS connection parameters are usually set after network registration
-    SerialMon.print(F("Connecting to "));
-    SerialMon.print(apn);
-    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-      SerialMon.println(" fail");
-      delay(10000);
-      return;
-    }
-    SerialMon.println(" success");
-
-  if (modem.isGprsConnected()) {
-    SerialMon.println("GPRS connected");
-  }
-#endif
-
-  // MQTT Broker setup
-  mqtt.setServer(broker, 1883);
-  mqtt.setCallback(mqttCallback);
+  setup_mqtt();
 }
 
 void loop() {
-
   if (!mqtt.connected()) {
-    SerialMon.println("=== MQTT NOT CONNECTED ===");
+    Serial.println("=== MQTT NOT CONNECTED ===");
     // Reconnect every 10 seconds
     uint32_t t = millis();
     if (t - lastReconnectAttempt > 10000L) {
@@ -271,4 +140,125 @@ void loop() {
   }
 
   mqtt.loop();
+}
+
+//======================================================================//
+// Functions
+
+void mqttCallback(char* topic, byte* payload, unsigned int len) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.write(payload, len);
+  Serial.println();
+
+  // Only proceed if incoming message's topic matches
+  if (String(topic) == topicLed) {
+    ledStatus = !ledStatus;
+    digitalWrite(LED_PIN, ledStatus);
+    mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
+  }
+}
+
+boolean mqttConnect() {
+  Serial.print("Connecting to ");
+  Serial.print(broker);
+
+  // Connect to MQTT Broker without authentication
+  // boolean status = mqtt.connect("Aquifera-ESP32-Client");
+
+  // Or, if you want to authenticate MQTT:
+  boolean status = mqtt.connect("Aquifera-ESP32-Client", mqtt_user, mqtt_pass);
+
+  if (status == false) {
+    Serial.println(" fail");
+    return false;
+  }
+  Serial.println(" success");
+  mqtt.publish(topicInit, "GsmClientTest started");
+  mqtt.subscribe(topicLed);
+  return mqtt.connected();
+}
+
+void setup_tinygsm() {
+  // Set GSM module baud rate
+  TinyGsmAutoBaud(SerialSim800L, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
+  // SerialSim800L.begin(9600);
+  delay(6000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  Serial.println("Initializing modem...");
+  modem.restart();
+  // modem.init();
+
+  String modemInfo = modem.getModemInfo();
+  Serial.print("Modem Info: ");
+  Serial.println(modemInfo);
+
+#if TINY_GSM_USE_GPRS
+  // Unlock your SIM card with a PIN if needed
+  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(GSM_PIN);
+  }
+#endif
+
+#if TINY_GSM_USE_WIFI
+    // Wifi connection parameters must be set before waiting for the network
+  Serial.print(F("Setting SSID/password..."));
+  if (!modem.networkConnect(wifiSSID, wifiPass)) {
+    Serial.println(" fail");
+    delay(10000);
+    return;
+  }
+  Serial.println(" success");
+#endif
+
+#if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_XBEE
+  // The XBee must run the gprsConnect function BEFORE waiting for network!
+  modem.gprsConnect(apn, gprsUser, gprsPass);
+#endif
+
+  Serial.print("Waiting for network...");
+  if (!modem.waitForNetwork()) {
+    Serial.println(" fail");
+    delay(10000);
+    return;
+  }
+  Serial.println(" success");
+
+  if (modem.isNetworkConnected()) {
+    Serial.println("Network connected");
+  }
+
+#if TINY_GSM_USE_GPRS
+  // GPRS connection parameters are usually set after network registration
+    Serial.print(F("Connecting to "));
+    Serial.print(apn);
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+      Serial.println(" fail");
+      delay(10000);
+      return;
+    }
+    Serial.println(" success");
+
+  if (modem.isGprsConnected()) {
+    Serial.println("GPRS connected");
+  }
+#endif
+}
+
+void setup_mqtt() {
+  // MQTT Broker setup
+  mqtt.setServer(broker, 1883);
+  mqtt.setCallback(mqttCallback);
+}
+
+void setup_wifi() {
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
 }
